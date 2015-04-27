5a3d4e30482434e76b4f16b20e77004dd4b7b653
Ismael Moral <jastertdc@icloud.com>
2015-04-27 12:43:43 +0200

    Added params to the sample

--- AnimeXML.py
+++ AnimeXML.py
@@ -6,28 +6,39 @@ import xml.etree.ElementTree as ElementTree
 # Import url library.
 from urllib.request import urlopen
 
+# Import sys library.
+import sys
+
 # XML to parse.
-sampleUrl = "http://cdn.animenewsnetwork.com/encyclopedia/api.xml?anime=16989"
+sampleUrl = "http://cdn.animenewsnetwork.com/encyclopedia/api.xml?anime="
 
-# Read the xml as a file.
-content = urlopen (sampleUrl)
+# Get the number of params we have in our application.
+params = len (sys.argv)
 
-# XML content is stored here to start working on it.
-xmlData = content.readall().decode('utf-8')
+# Check the number of params we have.
+if (params == 1):
+    print ("We need at least 1 anime identifier.")
+else:
+    for aid in range (1, params):
+        # Read the xml as a file.
+        content = urlopen (sampleUrl + sys.argv[aid])
 
-# Close the file.
-content.close()
+        # XML content is stored here to start working on it.
+        xmlData = content.readall().decode('utf-8')
 
-# Start parsing XML.
-root = ElementTree.fromstring (xmlData)
+        # Close the file.
+        content.close()
 
-for info in root.iter("anime"):
-    print ("Id: " + info.get("id"))
-    print ("Gid: " + info.get("gid"))
-    print ("Name: " + info.get("name"))
-    print ("Precision: " + info.get("precision"))
-    print ("Type: " + info.get("type"))
+        # Start parsing XML.
+        root = ElementTree.fromstring (xmlData)
 
-for info in root.iter ("info"):
-    if ("Vintage" in info.get("type")):
-        print ("Date: " + info.text)
+        for info in root.iter("anime"):
+            print ("Id: " + info.get("id"))
+            print ("Gid: " + info.get("gid"))
+            print ("Name: " + info.get("name"))
+            print ("Precision: " + info.get("precision"))
+            print ("Type: " + info.get("type"))
+
+        for info in root.iter ("info"):
+            if ("Vintage" in info.get("type")):
+                print ("Date: " + info.text)
